ae41eed301c5eff9475294014ac00218d2e88ae8
iChengc <591195975@qq.com>
2016-03-08 00:05:12 +0800

    mac & linux support
    Add schedule.sh script

---
+++ bin/scheduled.sh
@@ -0,0 +1,13 @@
+
+#echo "13 22 * * 1 ${script_dir}/Run.sh">>/etc/crontab 
+#!/bin/sh
+script_dir=$(cd "$(dirname "$0")"; pwd)
+echo $script_dir
+PROGRAM=$script_dir/Run.sh
+CRONTAB_CMD="15 23 * * * sh $PROGRAM > /dev/null 2>&1 &"
+(crontab -l 2>/dev/null | grep -Fv $PROGRAM; echo "$CRONTAB_CMD") | crontab -
+COUNT=`crontab -l | grep $PROGRAM | grep -v "grep"|wc -l `
+if [ $COUNT -lt 1 ]; then
+        echo "fail to add crontab $PROGRAM"
+        exit 1
+fi 
