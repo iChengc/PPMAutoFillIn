4b9251b7557be7d0611303d7532ae0d6e736d37b
Admin <Admin@Mini-Mac-OS-Builder.local>
2016-03-30 15:28:33 +0800

    fix a bug

--- PPM_Win.bat
+++ PPM_Win.bat
@@ -2,7 +2,9 @@
 COLOR 0A
 setlocal enabledelayedexpansion
 
-call account.ini
+rem change the user name and password to yours.
+set username=Your_user_name
+set password=Your_password
 
 set work_dir=%cd%
 set script_dir=%~dp0
@@ -10,8 +12,8 @@ set script_dir=%~dp0
 rem make sure the work directory is in where the script is.
 if not "%work_dir%"=="%script_dir%" cd /d "%script_dir%"
 
-echo set username=%username%>bin\config.ini
-echo set password=%password%>>bin\config.ini
+echo set username=%username%>bin\config.bat
+echo set password=%password%>>bin\config.bat
 
 rem copy the depency libs
 DEL bin\ppmauto_lib\* /Q /F /S
