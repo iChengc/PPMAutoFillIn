5cf37d8d5c5f9eac98e70b12f1a05d7e9ae8dfa2
iChengc <591195975@qq.com>
2016-05-17 19:11:31 +0800

    reset scheduled time

--- bin/scheduled.bat
+++ bin/scheduled.bat
@@ -1,4 +1,4 @@
 rem at 17:00 /every:F %cd%\Run.bat
 rem at 17:41 /every:M "C:\Program Files (x86)\Evernote\Evernote\Evernote.exe"
 rem echo %cd%
-schtasks /create /tn "PPMAuto" /tr %cd%\bin\run.bat /sc weekly /d FRI /st 17:00 /F
+schtasks /create /tn "PPMAuto" /tr %cd%\bin\run.bat /sc weekly /d FRI /st 12:00 /F
